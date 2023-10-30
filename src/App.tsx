abb71ad1c6df29cffdbd644a6dab5b11124e5b55
yena kwon <ewko0220@gmail.com>
2023-10-30 20:33:10 +0900

    feat(#2): font 설정 변경, pages 폴더 추가
    - App.tsx 에 리액트 쿼리 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 import { useState } from "react";
 import "./App.css";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <QueryClientProvider client={queryClient}>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+      </QueryClientProvider>
     </>
   );
 }
