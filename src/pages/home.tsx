368b3963d4dae00384e1616a5e20fb8a3eb0b5ba
yena kwon <ewko0220@gmail.com>
2024-05-23 22:16:37 +0900

    feat(#5): 불필요한 import 제거

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,9 @@
-// import { useQuery } from "react-query";
 import styled from "styled-components";
-// import { useCurrentLocation, useWatchLocation } from "../hooks/useGeoLocation";
-import GetWeather from "@/components/weather-data";
 import CityName from "@/components/city-name";
-// import OpenWeatherMap from "openweathermap-ts";
 
 const Home = () => {
-  // const { loc, error } = useCurrentLocation(geolocationOptions);
-  // getWeather();
-
-  GetWeather();
-
   return (
     <Container className="mx-auto my-auto">
-      <GetWeather />
       <CityName />
     </Container>
   );
