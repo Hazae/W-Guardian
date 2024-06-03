859c460eaec125c05fdbb2e429b52409efdb848d
yena kwon <ewko0220@gmail.com>
2024-06-03 20:54:14 +0900

    feat(#5): weather-data 파일 수정
    - tsx -> ts 파일로 수정
    - openWeather API 데이터 fetch 기능만 수행하도록 수정

---
+++ src/components/weather-data.ts
@@ -0,0 +1,29 @@
+import OpenWeatherMap from "openweathermap-ts";
+import { QueryFunctionContext } from "react-query";
+
+const apiKey = import.meta.env.VITE_WEATHER_KEY;
+
+export interface WeatherData {
+  name: string;
+}
+
+export const fetchWeather = async ({
+  queryKey,
+}: QueryFunctionContext): Promise<WeatherData> => {
+  const [key, { lat, lon }] = queryKey as [
+    string,
+    { lat: number; lon: number }
+  ];
+
+  const openWeather = await new OpenWeatherMap({
+    apiKey: apiKey,
+    units: "metric",
+    language: "KR"
+  });
+
+  const response = await openWeather.getCurrentWeatherByGeoCoordinates(
+    lat,
+    lon
+  );
+  return response;
+};
