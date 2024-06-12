4feab4c69e2e0ad1da4e6bb43bf88dcf567b23b2
yena kwon <ewko0220@gmail.com>
2024-06-12 22:04:57 +0900

    feat(#5): 날씨 아이콘 컴포넌트 수정
    - API 데이터에서 필요한 데이터 추출

--- src/components/weather-icons.tsx
+++ src/components/weather-icons.tsx
@@ -1,8 +1,22 @@
 import styled from "styled-components";
 import { WeatherProps } from "../type/types";
 
-export const WeatherIcons: React.FC<WeatherProps> = () => {
-  return <IconsCon></IconsCon>;
+export const WeatherIcons: React.FC<WeatherProps> = ({
+  weatherData,
+  isLoading,
+  error,
+}) => {
+  const icon = `<img src="images/${weatherData?.weather[0].icon}.png" alt="${weatherData?.weather[0].icon}" />`;
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>오류 발생: {error.message}</div>;
+
+  return <IconsCon>{icon}</IconsCon>;
 };
 
-const IconsCon = styled.div``;
+const IconsCon = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%);
+`;
