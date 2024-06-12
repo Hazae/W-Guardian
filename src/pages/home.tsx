6ed6a98c314589bf3ad4fef304460eca7398992d
yena kwon <ewko0220@gmail.com>
2024-06-12 22:04:24 +0900

    feat(#5): CSS 수정
    - attribute 에러 수정

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -16,14 +16,14 @@ const geolocationOptions = {
 const bgBright = "01d" || "02d" || "50d" || "50n";
 const bgCloudy = "03d" || "04d" || "10d";
 
-const Container = styled.div<{ bgColor: string }>`
+const Container = styled.div<{ $bgcolor?: string }>`
   width: 100vw;
   height: 100vh;
   font-size: 1rem;
   background-color: ${(props) =>
-    props.bgColor === bgBright
+    props.$bgcolor === bgBright
       ? "#f6e8cf"
-      : props.bgColor === bgCloudy
+      : props.$bgcolor === bgCloudy
       ? "#dbe1da"
       : "#233947"};
 `;
@@ -33,7 +33,7 @@ const Home: React.FC = () => {
   const { data, isLoading, error } = useWeather(loc?.latitude, loc?.longitude);
 
   return (
-    <Container className="mx-auto my-auto" bgColor={data?.weather[0].icon}>
+    <Container className="mx-auto my-auto" $bgcolor={data?.weather[0].icon}>
       <TodayDate />
       <CityName weatherData={data} isLoading={isLoading} error={error} />
       <WeatherIcons weatherData={data} isLoading={isLoading} error={error} />
