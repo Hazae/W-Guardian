8e984807ff79dee32edc82040438fd652fcf0670
yena kwon <ewko0220@gmail.com>
2024-06-24 22:11:15 +0900

    feat(#10): week 페이지 데이터 출력
    - 현재 시간대와 가까운 시간의 날씨 데이터 찾는 훅 생성
    - 데이터 콘솔 출력

--- src/pages/week.tsx
+++ src/pages/week.tsx
@@ -1,7 +1,8 @@
 import { useCurrentLocation } from "@/hooks/useGeoLocation";
 import { useWeathers } from "@/hooks/useWeather";
 import styled from "styled-components";
-import Header from "./header";
+// import Header from "./header";
+import useClosestWeather from "@/hooks/useClosestWeather";
 
 const geolocationOptions = {
   enableHighAccuracy: true,
@@ -13,16 +14,23 @@ const Week: React.FC = () => {
   const { loc } = useCurrentLocation(geolocationOptions);
   const { data, isLoading, error } = useWeathers(loc?.latitude, loc?.longitude);
 
+  const closestWeather = useClosestWeather(data);
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>오류 발생: {error.message}</div>;
+
   if (data) {
+    console.log(data);
+    console.log(closestWeather);
     return (
       <WeekCon>
         {/* <Header /> */}
         <div>
           {data.list.map((item) => (
-            <>
+            <div key={item.dt}>
               <div>날짜: {item.dt_txt}</div>
               <div>날씨: {item.weather[0].description}</div>
-            </>
+            </div>
           ))}
         </div>
       </WeekCon>
