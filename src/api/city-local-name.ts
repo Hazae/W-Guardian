f7f2ab29757b219ea0dcf3f80bc63f75d7a0db4d
yena kwon <ewko0220@gmail.com>
2024-06-24 20:09:43 +0900

    feat(#10): API 수정
    - 도시 이름 받아오는 API 수정
    - 날씨 데이터 API export 설정 수정

--- src/api/city-local-name.ts
+++ src/api/city-local-name.ts
@@ -5,11 +5,16 @@ const apiKey = import.meta.env.VITE_WEATHER_KEY;
 export const fetchGeocode = async (
   city: string | undefined
 ): Promise<string> => {
-  const response = await axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
-  );
+  const response = await axios
+    .get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+    )
+    .then((res) => {
+      return res.data[0];
+    })
+    .catch((err) => console.log(err));
 
-  const localName = response.data[0]?.local_names.ko;
+  const localName = response?.local_names.ko as string;
 
   if (!localName) {
     throw new Error("Korean local name not found in the geocode data");
