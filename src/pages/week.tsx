d0bd99b121a6f034f30001ca3db5cb0fe5954cf5
yena kwon <ewko0220@gmail.com>
2024-07-03 22:06:55 +0900

    feat(#10): week 페이지 데이터 출력
    - 원하는 시간대의 다른 날짜 데이터 출력
    - 날씨에 따른 배경색 CSS 적용

--- src/pages/week.tsx
+++ src/pages/week.tsx
@@ -10,10 +10,12 @@ const geolocationOptions = {
   maximumAge: 1000 * 3600 * 1, // 1시간
 };
 
+const bgBright = ["01d", "02d", "50d"];
+const bgCloudy = ["03d", "04d", "10d", "11d"];
+
 const Week: React.FC = () => {
   const { loc } = useCurrentLocation(geolocationOptions);
   const { data, isLoading, error } = useWeathers(loc?.latitude, loc?.longitude);
-
   const closestWeather = useClosestWeather(data);
 
   if (isLoading) return <div>로딩 중...</div>;
@@ -22,17 +24,32 @@ const Week: React.FC = () => {
   if (data) {
     console.log(data);
     console.log(closestWeather);
+
+    const dtTime = closestWeather?.dt_txt.split(" ")[1] as string;
+
+    const filteredWeather = data.list.filter((item) =>
+      item.dt_txt.includes(dtTime)
+    );
+
     return (
       <WeekCon>
         {/* <Header /> */}
-        <div>
-          {data.list.map((item) => (
-            <div key={item.dt}>
-              <div>날짜: {item.dt_txt}</div>
-              <div>날씨: {item.weather[0].description}</div>
-            </div>
-          ))}
-        </div>
+        {/* 터치 애니메이션 구현하기 */}
+        {filteredWeather.slice(0, 4).map((item, index) => (
+          <Box
+            key={item.dt}
+            className={`box ${index === 0 ? "focused" : ""}`}
+            onClick={(e) => {
+              const boxes = document.querySelectorAll(".box");
+              boxes.forEach((box) => box.classList.remove("focused"));
+              e.currentTarget.classList.add("focused");
+            }}
+            $bgcolor={item.weather[0].icon}
+          >
+            <div>날짜: {item.dt_txt}</div>
+            <div>날씨: {item.weather[0].description}</div>
+          </Box>
+        ))}
       </WeekCon>
     );
   }
@@ -42,6 +59,21 @@ const WeekCon = styled.div`
   width: 100vw;
   height: 100vh;
   font-size: 1rem;
+  display: flex;
+`;
+
+const Box = styled.div<{ $bgcolor: string }>`
+  width: 20%;
+  transition: width 0.3s ease;
+  background-color: ${(props) =>
+    (bgBright.includes(props.$bgcolor) && "#f6e8cf") ||
+    (bgCloudy.includes(props.$bgcolor) && "#dbe1da") ||
+    "#233947"};
+  box-shadow: 10px 0px 20px 2px rgba(0, 0, 0, 0.2);
+
+  &.focused {
+    width: 40%;
+  }
 `;
 
 export default Week;
