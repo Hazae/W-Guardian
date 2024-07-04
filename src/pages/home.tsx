38d523e7f5237e81c3e8bfb635fb0d066affea6b
yena kwon <ewko0220@gmail.com>
2024-07-04 21:34:38 +0900

    feat(#11): week 페이지 퍼블리싱
    - header 컴포넌트 추가
    - 내부 요소 구현

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,18 +5,19 @@ import TodayDate from "@/components/date";
 import { WeatherIcons } from "@/components/weather-icons";
 import WeatherDescription from "@/components/weather-description";
 import { useCurrentLocation } from "@/hooks/useGeoLocation";
-import useWeather from "@/hooks/useWeather";
+import { useCurWeather } from "@/hooks/useWeather";
+import Header from "./header";
 
 const geolocationOptions = {
   enableHighAccuracy: true,
   timeout: 1000 * 10, // 10초
-  maximumAge: 1000 * 3600 * 24, // 24시간
+  maximumAge: 1000 * 3600 * 1, // 1시간
 };
 
 const bgBright = ["01d", "02d", "50d"];
 const bgCloudy = ["03d", "04d", "10d", "11d"];
 const barNight = ["01n", "04n", "50n"];
-const barPosition = ["02d", "03d", "04d"];
+const barPosition = ["02d", "04d"];
 
 const Container = styled.div<{ $bgcolor: string }>`
   width: 100vw;
@@ -47,7 +48,6 @@ const Bar = styled.div<{ $colorposition: string }>`
     (barNight.includes(props.$colorposition) && "#a2d6ea") ||
     (props.$colorposition === "01d" && "#233947") ||
     (props.$colorposition === "02d" && "#233947") ||
-    (props.$colorposition === "03d" && "#233947") ||
     (props.$colorposition === "04d" && "#233947") ||
     "transparent"};
 `;
@@ -55,17 +55,24 @@ const Bar = styled.div<{ $colorposition: string }>`
 const TopText = styled.div`
   width: 40%;
   margin-left: 5%;
-  padding-top: 15%;
+  padding-top: 20%;
 `;
 
 const Home: React.FC = () => {
   const { loc } = useCurrentLocation(geolocationOptions);
-  const { data, isLoading, error } = useWeather(loc?.latitude, loc?.longitude);
+  const { data, isLoading, error } = useCurWeather(
+    loc?.latitude,
+    loc?.longitude
+  );
 
   const propsForCSS = data?.weather[0].icon as string;
 
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>오류 발생: {error.message}</div>;
+
   return (
     <Container className="mx-auto my-auto" $bgcolor={propsForCSS}>
+      <Header $textcolor={propsForCSS} />
       <TopText>
         <TodayDate />
         <CityName weatherData={data} isLoading={isLoading} error={error} />
--- src/pages/week.tsx
+++ src/pages/week.tsx
@@ -1,7 +1,7 @@
 import { useCurrentLocation } from "@/hooks/useGeoLocation";
 import { useWeathers } from "@/hooks/useWeather";
 import styled from "styled-components";
-// import Header from "./header";
+import Header from "./header";
 import useClosestWeather from "@/hooks/useClosestWeather";
 
 const geolocationOptions = {
@@ -22,7 +22,6 @@ const Week: React.FC = () => {
   if (error) return <div>오류 발생: {error.message}</div>;
 
   if (data) {
-    console.log(data);
     console.log(closestWeather);
 
     const dtTime = closestWeather?.dt_txt.split(" ")[1] as string;
@@ -32,25 +31,54 @@ const Week: React.FC = () => {
     );
 
     return (
-      <WeekCon>
-        {/* <Header /> */}
-        {/* 터치 애니메이션 구현하기 */}
-        {filteredWeather.slice(0, 4).map((item, index) => (
-          <Box
-            key={item.dt}
-            className={`box ${index === 0 ? "focused" : ""}`}
-            onClick={(e) => {
-              const boxes = document.querySelectorAll(".box");
-              boxes.forEach((box) => box.classList.remove("focused"));
-              e.currentTarget.classList.add("focused");
-            }}
-            $bgcolor={item.weather[0].icon}
-          >
-            <div>날짜: {item.dt_txt}</div>
-            <div>날씨: {item.weather[0].description}</div>
-          </Box>
-        ))}
-      </WeekCon>
+      <>
+        <Header $textcolor={closestWeather?.weather[0].icon as string} />
+        <WeekCon>
+          {filteredWeather.slice(0, 4).map((item, index) => (
+            <Box
+              key={item.dt}
+              className={`box ${index === 0 ? "focused" : ""}`}
+              onClick={(e) => {
+                const boxes = document.querySelectorAll(".box");
+                boxes.forEach((box) => box.classList.remove("focused"));
+                e.currentTarget.classList.add("focused");
+              }}
+              $bgcolor={item.weather[0].icon}
+              $idx={index}
+            >
+              <div className="date-text"></div>
+              <div className="temp-text">{Math.round(item.main.temp)}°</div>
+              <div className="icon-box">
+                <img
+                  src={`/img/${item.weather[0].icon}.png`}
+                  alt={`${item.weather[0].description}`}
+                  loading="lazy"
+                ></img>
+              </div>
+              <div className="week-description">
+                {(() => {
+                  if (item.weather[0].description.includes("튼"))
+                    return item.weather[0].description.replace("튼", "");
+                  else if (item.weather[0].description.includes("온"))
+                    return item.weather[0].description.replace("온", "");
+                  else if (item.weather[0].description.includes("실"))
+                    return item.weather[0].description.replace("실", "약한");
+                  else if (
+                    item.weather[0].description.includes(
+                      "약간의 구름이 낀 하늘"
+                    )
+                  )
+                    return item.weather[0].description.replace(
+                      "약간의 구름이 낀 하늘",
+                      "약간 흐림"
+                    );
+                  else return item.weather[0].description;
+                })()}
+              </div>
+            </Box>
+          ))}
+        </WeekCon>
+      </>
     );
   }
 };
@@ -59,20 +87,44 @@ const WeekCon = styled.div`
   width: 100vw;
   height: 100vh;
   font-size: 1rem;
-  display: flex;
+  position: relative;
+  // display: flex;
 `;
 
-const Box = styled.div<{ $bgcolor: string }>`
-  width: 20%;
-  transition: width 0.3s ease;
+const Box = styled.div<{ $bgcolor: string; $idx: number }>`
+  width: 35%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: ${(props) => props.$idx * 25}%;
+  transition: left 0.3s ease;
   background-color: ${(props) =>
     (bgBright.includes(props.$bgcolor) && "#f6e8cf") ||
     (bgCloudy.includes(props.$bgcolor) && "#dbe1da") ||
     "#233947"};
   box-shadow: 10px 0px 20px 2px rgba(0, 0, 0, 0.2);
+  padding-top: 10%;
+
+  .week-description {
+    text-align: center;
+    padding: 5%;
+    margin: 5% auto;
+    width: 90%;
+    background-color: rgba(255, 255, 255, 0.3);
+  }
 
   &.focused {
-    width: 40%;
+    // width: 60%;
+    left: ${(props) => props.$idx * 25 - 10}%;
+
+    // .icon-box {
+    //   width: 80%;
+    //   margin: 0 auto;
+    // }
+
+    .week-description {
+      background-color: rgba(255, 255, 255, 0.5);
+    }
   }
 `;
 
